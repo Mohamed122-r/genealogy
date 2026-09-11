f0f5b37a1a61e61884e051199f3ac17ff3239585
Mohamed122-r <anskajamal@gmail.com>
2026-09-11 12:26:34 +0200

    Update middleware config to restrict matcher to admin paths

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,5 @@ import { authConfig } from "@/lib/auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  // تشغيل الميدل وير على جميع المسارات باستثناء الملفات الثابتة والصور
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|fonts|images|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ttf|woff|woff2)$).*)",
-  ],
+  matcher: ["/admin/:path*"],
 };
